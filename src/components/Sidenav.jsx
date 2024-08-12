b115c8f996361a0463245fa11ab4634c75d9b315
Soban hanif <sobanatban4@gmail.com>
2024-08-12 22:09:19 +0500

    added font

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -1,6 +1,7 @@
 import { Box, Heading, HStack, Icon, Stack, Text } from "@chakra-ui/react"
 import { RxDashboard } from "react-icons/rx";
 import { RiArrowUpDownFill } from "react-icons/ri";
+import { BiSupport } from "react-icons/bi";
 
 
 
@@ -22,17 +23,40 @@ const Sidenav = () => {
 
 
     return (
-        <Stack boxShadow="sm" maxW="256px" h="100vh" bg="red">
-            <Heading textAlign="center" as="h1" fontSize="20px" pt="3.5rem">
-                WrathRex Code
-            </Heading>
-            <Box mt="6" mx="3">
-            {navLinks.map((nav) => (
-                <HStack mx="3" key={nav.text}>
-                    <Icon as={nav.icon} />
-                    <Text>{nav.text}</Text>
-                </HStack>
-            ))}
+        <Stack justify= "space-between" boxShadow="sm" maxW="256px" h="100vh">
+            <Box>
+                <Heading textAlign="center" as="h1" fontSize="20px" pt="3.5rem">
+                    WrathRex Code
+                </Heading>
+                <Box mt="6" mx="3">
+                    {navLinks.map((nav) => (
+                        <HStack mx="3" py="3" px="4" _hover={{
+                            borderRadius: "10px",
+                            bg: "#F3F3F7",
+                            color: "#171717",
+
+                        }}
+                            color="#797E82"
+                        >
+                            <Icon as={nav.icon} />
+                            <Text fontSize="14px" fontWeight="medium" >{nav.text}</Text>
+                        </HStack>
+                    ))}
+                </Box>
+            </Box>
+
+            <Box mt= "6" mx= "3" mb= "6">
+            <HStack mx="3" py="3" px="4" _hover={{
+                borderRadius: "10px",
+                bg: "#F3F3F7",
+                color: "#171717",
+
+            }}
+                color="#797E82"
+            >
+                <Icon as={BiSupport} />
+                <Text fontSize="14px" fontWeight="medium" >Support</Text>
+            </HStack>
             </Box>
         </Stack>
     )
