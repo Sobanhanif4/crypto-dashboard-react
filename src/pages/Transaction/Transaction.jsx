61590695ed2c011886ad18c73d8321b3e56270ae
Soban hanif <sobanatban4@gmail.com>
2024-08-22 21:17:36 +0500

    mobile card

--- src/pages/Transaction/Transaction.jsx
+++ src/pages/Transaction/Transaction.jsx
@@ -1,8 +1,25 @@
-import { Button, Card, Flex, Icon, Tab, TabIndicator, TabList, TabPanel, TabPanels, Tabs, Tag } from "@chakra-ui/react";
+import {
+  Button,
+  Card,
+  Flex,
+  HStack,
+  Icon,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  Tab,
+  TabIndicator,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+  Tag,
+} from "@chakra-ui/react";
 import DashboardLayout from "../../components/DashboardLayout";
 import { AiOutlineDownload } from "react-icons/ai";
 import TransactionTable from "./components/TransactionTable";
-
+import { BiSearch } from "react-icons/bi";
+import TransactionCardMob from "./components/TransactionCardMob";
 const TransactionPage = () => {
   const tabs = [
     {
@@ -20,39 +37,54 @@ const TransactionPage = () => {
     {
       name: "Trade",
       count: "22",
-    }
-  ]
-
+    },
+  ];
 
   return (
     <DashboardLayout>
       <Flex justify="end" mt="6" mb="3">
         <Button leftIcon={<Icon as={AiOutlineDownload} />}>Export CSV</Button>
       </Flex>
-      <Card>
+      <Card borderRadius="1rem">
         <Tabs>
-          <TabList py={4}>
-            {
-              tabs.map((tab) => (<Tab key={tab.name} display="flex" gap="2">{tab.name} <Tag colorScheme="gray" borderRadius="full">{tab.count}</Tag></Tab>))
-            }
-            
+          <TabList
+            pt={4}
+            display="flex"
+            w="full"
+            justifyContent="space-between"
+          >
+            <HStack>
+              {tabs.map((tab) => (
+                <Tab key={tab.name} display="flex" gap="2" pb={4}>
+                  {tab.name}{" "}
+                  <Tag colorScheme="gray" borderRadius="full">
+                    {tab.count}
+                  </Tag>
+                </Tab>
+              ))}
+            </HStack>
+
+            <InputGroup maxW="200px" pr={6}>
+              <InputLeftElement pointerEvents="none">
+                <Icon as={BiSearch} />
+              </InputLeftElement>
+              <Input type="tel" placeholder="Search..." />
+            </InputGroup>
           </TabList>
-          <TabIndicator
-        
-          />
+          <TabIndicator />
           <TabPanels>
             <TabPanel>
-              <TransactionTable /> 
+              <TransactionCardMob />
+              {/* <TransactionTable /> */}
             </TabPanel>
             <TabPanel>
-              <p>two!</p>
+              {/* <TransactionTable /> */}
             </TabPanel>
             <TabPanel>
-              <p>three!</p>
+              {/* <TransactionTable /> */}
             </TabPanel>
           </TabPanels>
         </Tabs>
-      
       </Card>
     </DashboardLayout>
   );
--- src/pages/Transaction/components/TransactionTable.jsx
+++ src/pages/Transaction/components/TransactionTable.jsx
@@ -1,66 +1,148 @@
 import {
-    Table,
-    Thead,
-    Tbody,
-    Tfoot,
-    Tr,
-    Th,
-    Td,
-    TableCaption,
-    TableContainer,
-  } from '@chakra-ui/react'
+  Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+  TableContainer,
+  Stack,
+  Text,
+  Tag,
+} from "@chakra-ui/react";
 const TransactionTable = () => {
-    const tableData = [
-        {
-            id: "HD82NA2H",
-            date: "20-Aug-2024",
-            time: "03:30 AM",
-            type: {
-                name: "PKR Deposite",
-                tag: "E-transfer",
-            },
-            amount: "+ PKR81,123",
-            status: "pending",
-        }
-    ]
+  const tableData = [
+    {
+      id: "HD82NA2H",
+      date: "2023-06-20",
+      time: "07:00 AM",
+      type: {
+        name: "PKR Deposit",
+        tag: "E-Transfer",
+      },
+      amount: "+PKR81,123",
+      status: "pending",
+    },
+    {
+      id: "HD82NA4H",
+      date: "2023-06-18",
+      time: "07:00 AM",
+      type: {
+        name: "PKR Widthdraw",
+        tag: "Wire Transfer",
+      },
+      amount: "-PKR55,123",
+      status: "processing",
+    },
+    {
+      id: "HD82NA5H",
+      date: "2023-06-18",
+      time: "07:00 AM",
+      type: {
+        name: "Buy",
+        tag: "BTC",
+      },
+      amount: "12.0554484 BTC",
+      status: "cancelled",
+    },
+    {
+      id: "HD82NA6H",
+      date: "2023-06-18",
+      time: "07:00 AM",
+      type: {
+        name: "Sell",
+        tag: "BTC",
+      },
+      amount: "-2.0554484 BTC",
+      status: "completed",
+    },
+    {
+      id: "HD82NA7H",
+      date: "2023-06-20",
+      time: "07:00 AM",
+      type: {
+        name: "BTC Deposit",
+      },
+      amount: "+15.5000000",
+      status: "pending",
+    },
+    {
+      id: "HD82NA8H",
+      date: "2023-06-18",
+      time: "07:00 AM",
+      type: {
+        name: "BTC Widthdraw",
+      },
+      amount: "-5.05555544",
+      status: "completed",
+    },
+  ];
+
+  const statusColor = {
+    pending: "#797E82",
+    processing: "#F5A508",
+    completed: "#059669",
+    cancelled: "#DC2626",
+  };
+
   return (
     <TableContainer>
-  <Table variant='simple'>
-    <TableCaption>Imperial to metric conversion factors</TableCaption>
-    <Thead>
-      <Tr>
-        <Th>To convert</Th>
-        <Th>into</Th>
-        <Th isNumeric>multiply by</Th>
-      </Tr>
-    </Thead>
-    <Tbody>
-      <Tr>
-        <Td>inches</Td>
-        <Td>millimetres (mm)</Td>
-        <Td isNumeric>25.4</Td>
-      </Tr>
-      <Tr>
-        <Td>feet</Td>
-        <Td>centimetres (cm)</Td>
-        <Td isNumeric>30.48</Td>
-      </Tr>
-      <Tr>
-        <Td>yards</Td>
-        <Td>metres (m)</Td>
-        <Td isNumeric>0.91444</Td>
-      </Tr>
-    </Tbody>
-    <Tfoot>
-      <Tr>
-        <Th>To convert</Th>
-        <Th>into</Th>
-        <Th isNumeric>multiply by</Th>
-      </Tr>
-    </Tfoot>
-  </Table>
-</TableContainer>
-  )
-}
+      <Table variant="simple">
+        <Thead>
+          <Tr>
+            <Th>ID</Th>
+            <Th>Date & Time</Th>
+            <Th>Type</Th>
+            <Th>Amount</Th>
+            <Th>Status</Th>
+          </Tr>
+        </Thead>
+        <Tbody color="p.black">
+          {tableData.map((data) => (
+            <Tr key={data.id}>
+              <Td fontSize="sm" fontWeight="medium">
+                {data.id}
+              </Td>
+              <Td>
+                <Stack>
+                  <Text alignItems="left" fontSize="sm" fontWeight="medium">
+                    {data.date}
+                  </Text>
+                  <Text fontSize="xs" color="black.60">
+                    {data.time}
+                  </Text>
+                </Stack>
+              </Td>
+              <Td>
+                <Stack>
+                  <Text fontSize="sm" fontWeight="medium">
+                    {data.type.name}
+                  </Text>
+                  <Text fontSize="xs" color="black.60">
+                    {data.type?.tag}
+                  </Text>
+                </Stack>
+              </Td>
+              <Td fontSize="sm" fontWeight="medium">
+                {data.amount}
+              </Td>
+              <Td fontSize="sm" fontWeight="medium">
+                <Tag
+                  bg={statusColor[data.status]}
+                  color="white"
+                  borderRadius="full"
+                >
+                  {data.status}
+                </Tag>
+              </Td>
+            </Tr>
+          ))}
+        </Tbody>
+      </Table>
+    </TableContainer>
+  );
+};
 
-export default TransactionTable
+export default TransactionTable;
